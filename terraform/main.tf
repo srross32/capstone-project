021992da8fc95e353caeb6c0c2108d064ebe23f8
Hunter Ross <hrross@hrross.net>
2025-02-26 13:27:37 -0600

    Terraform changes and creation of dockerfile

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,9 @@ resource "aws_instance" "backend_server" {
               yum install docker -y
               systemctl start docker
               systemctl enable docker
-              docker run -d -p 3000:3000 voting-app
+              $(aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 061039770229.dkr.ecr.us-east2.amazonaws.com)
+              docker pull 061039770229.dkr.ecr.us-east-2.amazonaws.com/secure-voting-backend:latest
+              docker run -d -p 3000:3000 061039770229.dkr.ecr.us-east-2.amazonaws.com/secure-voting-backend:latest
               EOF
 
   metadata_options {
@@ -26,15 +28,63 @@ resource "aws_instance" "backend_server" {
 
 resource "aws_s3_bucket" "frontend_bucket" {
   bucket = var.voting_s3_bucket
+}
+
+resource "aws_s3_bucket_website_configuration" "frontend_bucket_website" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket-acl" {
+  bucket = aws_s3_bucket.frontend_bucket.id
   acl    = "public-read"
+  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
+}
 
-  website {
-    index_document = "index.html"
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+  depends_on = [aws_s3_bucket_public_access_block.public-access-block]
+}
+
+resource "aws_s3_bucket_public_access_block" "public-access-block" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "bucket-policy" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  policy = data.aws_iam_policy_document.iam-policy-1.json
+}
+data "aws_iam_policy_document" "iam-policy-1" {
+  statement {
+    sid    = "AllowPublicRead"
+    effect = "Allow"
+resources = [
+      "arn:aws:s3:::${aws_s3_bucket.frontend_bucket.id}",
+      "arn:aws:s3:::${aws_s3_bucket.frontend_bucket.id}/*",
+    ]
+actions = ["S3:GetObject"]
+principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
   }
 
-  tags = {
-    Name = "SecureVotingApp-Frontend"
-  }
+  depends_on = [aws_s3_bucket_public_access_block.public-access-block]
 }
 
 resource "aws_db_instance" "rds" {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,7 +10,7 @@ variable "instance_type" {
 
 variable "db_username" {
   description = "Database username"
-  default     = "admin"
+  default     = "apiuser"
 }
 
 variable "db_password" {
